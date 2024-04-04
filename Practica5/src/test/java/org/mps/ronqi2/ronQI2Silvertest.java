90a8a265ac6681737551de18ec4bc9aac532e911
tbms18 <124881416+tbms18@users.noreply.github.com>
2024-04-04 12:22:25 +0200

    inicialización mock

--- Practica5/src/test/java/org/mps/ronqi2/ronQI2Silvertest.java
+++ Practica5/src/test/java/org/mps/ronqi2/ronQI2Silvertest.java
@@ -1,35 +1,60 @@
 package org.mps.ronqi2;
 
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.mps.dispositivo.Dispositivo;
 
 public class ronQI2Silvertest {
 
-    
+    RonQI2 ronqi2;
+    Dispositivo dispositivo;
+
+    @BeforeEach
+    public void setUp() {
+        dispositivo = mock(Dispositivo.class);
+        ronqi2 = new RonQI2Silver();
+    }
+
     /*
-     * Analiza con los caminos base qué pruebas se han de realizar para comprobar que al inicializar funciona como debe ser. 
-     * El funcionamiento correcto es que si es posible conectar ambos sensores y configurarlos, 
-     * el método inicializar de ronQI2 o sus subclases, 
-     * debería devolver true. En cualquier otro caso false. Se deja programado un ejemplo.
+     * Analiza con los caminos base qué pruebas se han de realizar para comprobar
+     * que al inicializar funciona como debe ser.
+     * El funcionamiento correcto es que si es posible conectar ambos sensores y
+     * configurarlos,
+     * el método inicializar de ronQI2 o sus subclases,
+     * debería devolver true. En cualquier otro caso false. Se deja programado un
+     * ejemplo.
      */
-    
+
     /*
-     * Un inicializar debe configurar ambos sensores, comprueba que cuando se inicializa de forma correcta (el conectar es true), 
+     * Un inicializar debe configurar ambos sensores, comprueba que cuando se
+     * inicializa de forma correcta (el conectar es true),
      * se llama una sola vez al configurar de cada sensor.
      */
 
     /*
-     * Un reconectar, comprueba si el dispositivo desconectado, en ese caso, conecta ambos y devuelve true si ambos han sido conectados. 
-     * Genera las pruebas que estimes oportunas para comprobar su correcto funcionamiento. 
-     * Centrate en probar si todo va bien, o si no, y si se llama a los métodos que deben ser llamados.
+     * Un reconectar, comprueba si el dispositivo desconectado, en ese caso, conecta
+     * ambos y devuelve true si ambos han sido conectados.
+     * Genera las pruebas que estimes oportunas para comprobar su correcto
+     * funcionamiento.
+     * Centrate en probar si todo va bien, o si no, y si se llama a los métodos que
+     * deben ser llamados.
      */
-    
+
     /*
-     * El método evaluarApneaSuenyo, evalua las últimas 5 lecturas realizadas con obtenerNuevaLectura(), 
-     * y si ambos sensores superan o son iguales a sus umbrales, que son thresholdP = 20.0f y thresholdS = 30.0f;, 
-     * se considera que hay una apnea en proceso. Si hay menos de 5 lecturas también debería realizar la media.
+     * El método evaluarApneaSuenyo, evalua las últimas 5 lecturas realizadas con
+     * obtenerNuevaLectura(),
+     * y si ambos sensores superan o son iguales a sus umbrales, que son thresholdP
+     * = 20.0f y thresholdS = 30.0f;,
+     * se considera que hay una apnea en proceso. Si hay menos de 5 lecturas también
+     * debería realizar la media.
      * /
-     
-     /* Realiza un primer test para ver que funciona bien independientemente del número de lecturas.
-     * Usa el ParameterizedTest para realizar un número de lecturas previas a calcular si hay apnea o no (por ejemplo 4, 5 y 10 lecturas).
-     * https://junit.org/junit5/docs/current/user-guide/index.html#writing-tests-parameterized-tests
+     * 
+     * /* Realiza un primer test para ver que funciona bien independientemente del
+     * número de lecturas.
+     * Usa el ParameterizedTest para realizar un número de lecturas previas a
+     * calcular si hay apnea o no (por ejemplo 4, 5 y 10 lecturas).
+     * https://junit.org/junit5/docs/current/user-guide/index.html#writing-tests-
+     * parameterized-tests
      */
 }
